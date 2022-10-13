851d7e5c85d101fdadd65ef9840360c4ca7662fe
ridaoc19 <ridaoc19@gmail.com>
2022-10-13 16:08:47 +0200

    actualizacion ruta

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -25,53 +25,8 @@ export async function getClient() {
 }
 
 export async function postClient() {
-  let {data} = axios.get('https://ipgeolocation.abstractapi.com/v1/?api_key=578130a355254e108446d95e45a74c84')
 
-  // let data = {
-  //   ip_address: "83.43.130.56",
-  //   city: "Tarragona",
-  //   city_geoname_id: 3108288,
-  //   region: "Catalonia",
-  //   region_iso_code: "CT",
-  //   region_geoname_id: 3336901,
-  //   postal_code: "43007",
-  //   country: "Spain",
-  //   country_code: "ES",
-  //   country_geoname_id: 2510769,
-  //   country_is_eu: true,
-  //   continent: "Europe",
-  //   continent_code: "EU",
-  //   continent_geoname_id: 6255148,
-  //   longitude: 1.254,
-  //   latitude: 41.1185,
-  //   security: {
-  //     is_vpn: false,
-  //   },
-  //   timezone: {
-  //     name: "Europe/Madrid",
-  //     abbreviation: "CEST",
-  //     gmt_offset: 2,
-  //     current_time: "20:30:06",
-  //     is_dst: true,
-  //   },
-  //   flag: {
-  //     emoji: "🇪🇸",
-  //     unicode: "U+1F1EA U+1F1F8",
-  //     png: "https://static.abstractapi.com/country-flags/ES_flag.png",
-  //     svg: "https://static.abstractapi.com/country-flags/ES_flag.svg",
-  //   },
-  //   currency: {
-  //     currency_name: "Euros",
-  //     currency_code: "EUR",
-  //   },
-  //   connection: {
-  //     autonomous_system_number: 3352,
-  //     autonomous_system_organization: "Telefonica De Espana S.a.u.",
-  //     connection_type: "Corporate",
-  //     isp_name: "Telefonica de Espana SAU",
-  //     organization_name: "RIMA (Red IP Multi Acceso)",
-  //   },
-  // };
+  let {data} = await axios.get('https://ipgeolocation.abstractapi.com/v1/?api_key=578130a355254e108446d95e45a74c84')
 
   let ip = {
     ip_address: data.ip_address,
@@ -110,8 +65,8 @@ export async function postClient() {
     organization_name: data.connection.organization_name,
   };
 
-  // fetch("http://localhost:3001/count", {
-    fetch('https://portafolio-w1tt.onrender.com/count', {
+  fetch("http://localhost:3001/count", {
+    // fetch('https://portafolio-w1tt.onrender.com/count', {
     method: "POST",
     body: JSON.stringify(ip),
     headers: {
@@ -122,6 +77,10 @@ export async function postClient() {
     .then((res) => console.log(res))
     .catch((error) => console.log(error));
 }
+
+
+
+
 
 // https://ipgeolocation.abstractapi.com/v1/?api_key=578130a355254e108446d95e45a74c84
 
