3bbdc2e091bab7ebdb0d3707f8c4b9469eaa70df
ridaoc19 <ridaoc19@gmail.com>
2023-05-01 13:50:16 -0500

    feat: button information

--- client/src/views/Admin/sections/Company/Render/Render.jsx
+++ client/src/views/Admin/sections/Company/Render/Render.jsx
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { svg } from "../../../../../components/assets/svg";
 import Tooltip from "../../../../../components/Layout/Tooltip/Tooltip";
 
 function Render({ handleOnClick, company, status }) {
+  const [tooltip, setTooltip] = useState(true)
 
   let render = status.company_position_id
     ? [company?.find((s) => s._id === status.company_position_id)]
     : company;
 
+  const information = [
+  {type: "edit",data :"Eleminaría la empresa y toda la información relacionada con ella"},
+  {type: "delete",data : "Agrega nuevos cargos o grados a la Empresa"},
+  {type: "add",data : "si le da clic al nombre de la empresa, le da la opción de editar sus campos"}]
+
+  const handleOnMouseDown = (e) => {
+    e.preventDefault();
+
+  }
+
+  const handleOnMouseUp = (e) => {
+    e.preventDefault();
+
+  }
+
   return (
     <>
       <ul>
+        {tooltip && <li>{information?.map( i => 
+        <i key={i.type} value={i.data} onMouseDown={handleOnMouseDown} onMouseUp={handleOnMouseUp}>
+         <Tooltip text={i.data} color={"blue"} position="right">
+          {svg({ type: "information" })}
+          </Tooltip> 
+          </i>)}</li>}
         {render?.map((e) => (
           <li key={e._id}>
             <Tooltip text={`Editar ${e.name}`} color={"blue"} position="right">
@@ -19,8 +41,8 @@ function Render({ handleOnClick, company, status }) {
             <Tooltip text={`Eliminar ${e.name}`} color={"red"} position="top">
               <button id="company_delete" name="company_delete" onClick={handleOnClick} value={e._id}>{svg({ type: "delete", color: "red" })}</button>
             </Tooltip>
-            <Tooltip text={`Agregar Cargo o grado desmpeñado en ${e.name}`} color={"green"} position="left">
-              <button id="company_add_position" name="company_add_position" onClick={handleOnClick} value={e._id}>{svg({ type: "delete", color: "green" })}</button>
+            <Tooltip text={`Agregar Cargo o grado desmpeñado en ${e.name}`} color={"green"} position="bottom">
+              <button id="company_add_position" name="company_add_position" onClick={handleOnClick} value={e._id}>{svg({ type: "add_position", color: "green" })}</button>
             </Tooltip>
             {e.name}
           </li>
