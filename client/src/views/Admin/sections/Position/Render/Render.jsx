0cbe692c5c209dba9976a0165ff3e1de1b509349
ridaoc19 <ridaoc19@gmail.com>
2023-05-06 04:50:52 -0500

    feat: style functions

--- client/src/views/Admin/sections/Position/Render/Render.jsx
+++ client/src/views/Admin/sections/Position/Render/Render.jsx
@@ -9,19 +9,19 @@ function Render({ handleOnClick, state, status }) {
     ? [state?.position?.find((s) => s._id === status.position_function_id)]
     : state?.position;
 
-    const information = [
-      { type: "edit", color: "blue", data: "Puede Editar los campos del Cargo o Grado" },
-      { type: "delete", color: "red", data: "Eleminaría la empresa y toda la información relacionada con ella" },
-      { type: "add", color: "green", data: "si le da clic al nombre de la empresa, abre la opción para agregar nuevos cargos o grados a la Empresa" }]
-  
-      useEffect(() => {
-        window.matchMedia("(min-width: 1200px)").matches ? setTooltip(false) : setTooltip(true)
-      }, [])
+  const information = [
+    { type: "edit", color: "blue", data: "Puede Editar los campos del Cargo o Grado" },
+    { type: "delete", color: "red", data: "Eleminaría el Cargo o Grado" },
+    { type: "add", color: "green", data: "Al darle clic al el Cargo o Grado, abre la opción para agregar Funciones o Proyectos" }]
+
+  useEffect(() => {
+    window.matchMedia("(min-width: 1200px)").matches ? setTooltip(false) : setTooltip(true)
+  }, [])
 
   return (
     <>
       <ul>
-      {tooltip && <li>{information?.map(i =>
+        {tooltip && <li>{information?.map(i =>
           <i key={i.type} value={i.data} >
             <Tooltip text={i.data} color={i.color} position="right">
               {svg({ type: "information", color: i.color, width: 18 })}
@@ -31,28 +31,28 @@ function Render({ handleOnClick, state, status }) {
           return (
             <li key={e._id}>
               {!tooltip && <>
-              <Tooltip text={`Editar ${e.name}`} color={"blue"} position="right">
-                <i id="position_edit" name="position_edit" onClick={handleOnClick} value={e._id}>{svg({ type: "edit" })}</i>
-              </Tooltip><Tooltip text={`Eliminar ${e.name}`} color={"red"} position="top">
-                <i id="position_delete" name="position_delete" onClick={handleOnClick} value={e._id}>{svg({ type: "delete", color: "red" })}</i>
-              </Tooltip><Tooltip text={`Agregar Cargo o grado desmpeñado en ${e.name}`} color={"green"} position="bottom">
-                <i id="position_add_position" name="position_add_position" onClick={handleOnClick} value={e._id}>{e.name}</i>
-              </Tooltip>
-            </>}
+                <Tooltip text={`Edita ${e.name}`} color={"blue"} position="right">
+                  <i id="position_edit" name="position_edit" onClick={handleOnClick} value={e._id}>{svg({ type: "edit" })}</i>
+                </Tooltip><Tooltip text={`Eliminaria ${e.name}`} color={"red"} position="top">
+                  <i id="position_delete" name="position_delete" onClick={handleOnClick} value={e._id}>{svg({ type: "delete", color: "red" })}</i>
+                </Tooltip><Tooltip text={`Agrega Funciones o Proyectos de ${e.name}`} color={"green"} position="bottom">
+                  <i id="position_add_position" name="position_add_position" onClick={handleOnClick} value={e._id}>{e.name}</i>
+                </Tooltip>
+              </>}
 
-            {tooltip && <>
-              <i id="position_edit" name="position_edit" onClick={handleOnClick} value={e._id}>{svg({ type: "edit" })}</i>
-              <i id="position_delete" name="position_delete" onClick={handleOnClick} value={e._id}>{svg({ type: "delete", color: "red" })}</i>
-              <i id="position_add_position" name="position_add_position" onClick={handleOnClick} value={e._id}>{e.name}</i>
-            </>}
-             </li>
+              {tooltip && <>
+                <i id="position_edit" name="position_edit" onClick={handleOnClick} value={e._id}>{svg({ type: "edit" })}</i>
+                <i id="position_delete" name="position_delete" onClick={handleOnClick} value={e._id}>{svg({ type: "delete", color: "red" })}</i>
+                <i id="position_add_position" name="position_add_position" onClick={handleOnClick} value={e._id}>{e.name}</i>
+              </>}
+            </li>
           );
         })}
 
         {status.position_add && <li>
           <button id="position_add" name="position_add" onClick={handleOnClick}>{svg({ type: "add", color: "rgb(5, 207, 5)" })} nuevo Cargo o Grado</button>
         </li>}
-        
+
       </ul>
     </>
   );
