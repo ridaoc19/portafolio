0b673a297cb9e4940e457b682112acdcbf077534
ridaoc19 <ridaoc19@gmail.com>
2023-05-25 12:31:50 -0500

    fix: correction start modal home location

--- client/src/views/Home/ModalScroll/ModalScroll.jsx
+++ client/src/views/Home/ModalScroll/ModalScroll.jsx
@@ -3,15 +3,26 @@ import React from "react";
 function ModalScroll(props) {
   return (
     <>
-      <h2>Que puedes hacer</h2>
-      <h3>Contenido del autor</h3>
+      <h2>Que puedes hacer en mi pagina</h2>
+
+      <h3>Contenido de la pagina</h3>
       <p>Puedes ver mi historia laboral y educativa</p>
-      <h3>Registrar información del usuario</h3>
+      <hr />
+
+      <h3>Contenido de la pagina</h3>
+      <p>Todo lo que encuentra de la siguiente forma son botones o link para redirigirte a ver mas información</p>
+      <ul>
+        <li><button>botones</button></li>
+        <li> <a href="/#">@redirección</a></li>
+      </ul>
+      <hr />
+
+      <h3>Registrar información</h3>
       <p>
-        Puedes iniciar sesión con Google, ir Admin y registrar información de
-        prueba, los campos tienen validaciones, cuando vuelvas al Home, en la
-        parte superior derecha, encuentras un selector, ahi cambia de mi
-        Portafolio a la sección creada por usted, la información que almacene,
+        Puedes iniciar sesión con Google en <a href="/#">Login</a> e ir <a href="/#">Admin</a> y registrar información de
+        prueba, los campos tienen validaciones, cuando vuelvas al <a href="/#">Home</a>, en la
+        parte superior derecha, encuentras un <select><option value="">Ejemplo</option></select>, ahi cambia de
+        {" "}<select><option value=""> Ricardo Dav... </option></select> a la sección creada por usted, la información que almacene,
         yo la iré depurando con el tiempo, gracias por tomarse el tiempo de visitar mi Portafolio
       </p>
     </>
--- client/src/views/Start/Location/Location.jsx
+++ client/src/views/Start/Location/Location.jsx
@@ -1,10 +1,25 @@
 import { Navigate } from "react-router-dom";
 import useLoading from "../../../components/hooks/custom/useLoading";
+import { useEffect, useState } from "react";
 
 function Location({ location }) {
-  const loading = useLoading(12000);
+  const loading = useLoading(15000);
+  const [value, setValue] = useState(0)
 
   const { city, continent, country, region } = location[0];
+
+  useEffect(() => {
+    let width = 1;
+    let id = setInterval(frame, 147);
+    function frame() {
+      if (width >= 101) {
+        clearInterval(id);
+      } else {
+        setValue(width)
+        width++;
+      }
+    }
+  }, [])
 
   return (
     <>
@@ -26,6 +41,7 @@ function Location({ location }) {
               </div>
             </div>
             <div className="location__especification">
+              <div><span>{`${value}%`}</span><progress value={value} max="100">{value}</progress></div>
               <h3>{location[1]} visitas desde</h3>
               {location[2]?.map((e, i) => (
                 <p key={i}><span>{e.count}</span> <img src={e.svg} alt="" width={20} /><span>{e.country}</span><span>{e.region}</span><span>{e.city}</span></p>
